61087a1428d68a0fcbedb714665b46ff55d49d32
ML expert <vasile123andronic@gmail.com>
2018-11-22 06:01:16 -0800

    ocr by KNN

--- ocrKNN.py
+++ ocrKNN.py
@@ -1,7 +1,7 @@
-# import numpy as np
+import numpy as np
 # import cv2 as cv
-# from matplotlib import pyplot as plt
-# img = cv.imread('./ocr/digits.png')
+from matplotlib import pyplot as plt
+# img = cv.imread('digits.png')
 # gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 # # Now we split the image to 5000 cells, each 20x20 size
 # cells = [np.hsplit(row,100) for row in np.vsplit(gray,50)]
@@ -27,3 +27,76 @@
 # # save the data
 # np.savez('knn_data.npz',train=train, train_labels=train_labels)
 #
+
+import cv2
+from imutils.video import VideoStream
+import time
+import imutils
+
+with np.load('knn_data.npz') as data:
+    print
+    data.files
+    train = data['train']
+    train_labels = data['train_labels']
+
+knn = cv2.ml.KNearest_create()
+knn.train(train, cv2.ml.ROW_SAMPLE, train_labels)
+
+cap=cv2.VideoCapture(0)
+time.sleep(2.0)
+
+while(True):
+    # Capture frame-by-frame
+    ret1, frame = cap.read()
+
+    # Our operations on the frame come here
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+
+    frame = cv2.medianBlur(frame, 5)
+
+    ret2, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)
+
+    # src.create(rows, cols, CV_8UC1);
+    # src = imread(your - file, CV_8UC1);
+
+
+    # source_thresh = cv2.adaptiveThreshold(frame, 255, 0, 1, 5, 2)
+    frame = cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+    filter = cv2.createBackgroundSubtractorMOG2()
+    frame = filter.apply(frame)
+
+    # frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+    frame = cv2.bitwise_not(frame, cv2.COLOR_GRAY2RGB)
+    im2, contours, hierarchy = cv2.findContours(frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(frame, contours, 2, (0, 255, 0), 3)
+
+    cv2.imshow('thresh', frame)
+
+    test_img = cv2.resize(frame, (20, 20))
+    x = np.array(test_img)
+    test_img = x.reshape(-1, 400).astype(np.float32)
+    ret, result, neighbours, dist = knn.findNearest(test_img, k=1)
+    # Print the predicted number
+    print(int(result[0]))
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
+
+#
+# test_img = cv2.imread("test.jpg")
+# test_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+# test_img = cv2.resize(test_img, (20, 20))
+# x = np.array(test_img)
+# test_img = x.reshape(-1, 400).astype(np.float32)
+# ret, result, neighbours, dist = knn.findNearest(test_img, k=1)
+# # Print the predicted number
+# print(int(result))
